09407a6cc254fe9eba0d78feb7f0b4a3922b86b5
edu ruiz <eduardo.ruiz@eokoe.com>
2019-01-14 10:54:21 -0200

    Add eslint disable rule specification

--- assets/assets/scripts/index.js
+++ assets/assets/scripts/index.js
@@ -6631,12 +6631,11 @@ function initContactForm() {
   const phone = document.getElementById('phone');
   const maskOptions = {
     mask: '+00 (00) 000000000'
-  }; // eslint-disable-next-line
-
-  let mask;
+  };
 
   if (phone) {
-    mask = new _imask2.default(phone, maskOptions);
+    // eslint-disable-next-line
+    const mask = new _imask2.default(phone, maskOptions);
   }
 
   function clearPhone(dirtyPhone) {
