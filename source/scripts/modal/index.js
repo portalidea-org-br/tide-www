6930a5969c7074d3f88d6d1c14651b7a270d5917
eduruiz <contato@eduruiz.com.br>
2019-06-19 20:07:27 +0000

    Fix modal close bug when playing youtube video

--- source/scripts/modal/index.js
+++ source/scripts/modal/index.js
@@ -4,12 +4,24 @@ function setModalGlobalStatus() {
   sessionStorage.setItem('modalHasBeenClosed', 1);
 }
 
+function handleModalClose(modalId) {
+  if (modalId === 'js-modal-idea-video') {
+    const iframeYoutube = document.querySelector('#iframeYoutube');
+    iframeYoutube.contentWindow.postMessage('{"event":"command", "func":"pauseVideo", "args":""}', '*');
+  }
+  if (modalId === 'js-modal-user-popup') {
+    setModalGlobalStatus(1); // [2]
+  }
+}
+
 export default function initModal() {
   const $modalLink = document.getElementById('js-open-modal');
 
   MicroModal.init({
     // debugMode: true,
-    onClose: () => setModalGlobalStatus(1), // [2]
+    onClose: (modal) => {
+      handleModalClose(modal.id);
+    },
   });
 
   if ($modalLink && !sessionStorage.getItem('modalHasBeenClosed')) {
