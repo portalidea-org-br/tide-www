784e894a3610de0332959e6803b1e151496a7e28
eduruiz <contato@eduruiz.com.br>
2019-06-24 12:38:35 -0300

    Temporarily change api verification

--- source/scripts/config.js
+++ source/scripts/config.js
@@ -1,6 +1,6 @@
 export default {
   api: {
-    domain: (window.location.hostname.indexOf('portalidea.org.br') !== -1
+    domain: (window.location.hostname.indexOf('https://portalidea.org.br/') !== -1
       ? 'https://api.portalidea.org.br/'
       : 'https://dapitide.eokoe.com/api/'),
   },
