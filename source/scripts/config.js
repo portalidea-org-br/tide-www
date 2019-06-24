a9e25cde10d96d225dd062632c7b29b33a137e0c
eduruiz <contato@eduruiz.com.br>
2019-06-24 12:49:59 -0300

    Fix prod api path

--- source/scripts/config.js
+++ source/scripts/config.js
@@ -1,7 +1,7 @@
 export default {
   api: {
-    domain: (window.location.hostname.indexOf('https://portalidea.org.br/') !== -1
-      ? 'https://api.portalidea.org.br/'
+    domain: (window.location.hostname.indexOf('portalidea.org.br') !== -1
+      ? 'https://api.portalidea.org.br/api'
       : 'https://dapitide.eokoe.com/api/'),
   },
 };
